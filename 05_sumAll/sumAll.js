a18cf0250d83e8354f9652ec696d348043bc7fdb
Dylan <dmurayama93@gmail.com>
2023-09-22 09:49:14 -1000

    rename checkNaN bool to isNumber in 05 sum all

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,15 +1,15 @@
 const sumAll = function(...numbers) {
     numbers.sort();
-    let checkNaN = false;
+    let isNumber = false;
     // const newArray = [];
     for (let i = 0; i <= numbers.length -1; i++) {
         if (!Number.isInteger(numbers[i])){
-            checkNaN = true;
+            isNumber = true;
             return "ERROR";
         }
     }
 
-    if (numbers[0] >= 0 && !checkNaN) {
+    if (numbers[0] >= 0 && !isNumber) {
         let j = numbers[0];
 
         for (let i = numbers[0] + 1; i <= numbers[numbers.length - 1]; i++) {
