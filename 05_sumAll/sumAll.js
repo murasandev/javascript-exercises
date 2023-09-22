b6b556d013857bf7314ada157930fc4f907918a5
Dylan <dmurayama93@gmail.com>
2023-09-22 09:55:17 -1000

    clean code 05 sum all

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,25 +1,20 @@
 const sumAll = function(...numbers) {
     numbers.sort();
     let isNumber = false;
-    // const newArray = [];
+
     for (let i = 0; i <= numbers.length -1; i++) {
-        if (!Number.isInteger(numbers[i])){
-            isNumber = true;
-            return "ERROR";
-        }
+        if (!Number.isInteger(numbers[i])) isNumber = true;
     }
 
     if (numbers[0] >= 0 && !isNumber) {
-        let j = numbers[0];
+        let sum = numbers[0];
 
         for (let i = numbers[0] + 1; i <= numbers[numbers.length - 1]; i++) {
-            j += i;
+            sum += i;
         }
-        return j;
+        return sum;
     }
-    else {
-        return "ERROR";
-    }
+    else return "ERROR";
 };
 
 // Do not edit below this line
