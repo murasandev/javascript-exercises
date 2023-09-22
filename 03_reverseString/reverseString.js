fd52c95fe5a5398b893e097385075843c61de369
Dylan <dmurayama93@gmail.com>
2023-09-21 19:34:20 -1000

    clean up comments

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,10 +1,7 @@
 const reverseString = (string) => {
-// take string and store each character into array
-// slice off last letter of array and store in new array
-//return new array
     const stringArray = string.split("");
     const reverseArray = [];
-    
+
     for (let i = stringArray.length - 1, j = 0; i >= 0; i--) {
         reverseArray[j] = stringArray[i];
         j++;
