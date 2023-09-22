7b1073cfaae4e739d4c523073abeb13008c30fb3
Dylan <dmurayama93@gmail.com>
2023-09-21 18:27:39 -1000

    complete 02-repeatString.js

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
-
+const repeatString = (string, num) => {
+    let randNum = Math.floor(Math.random() * num);
+    if (randNum >= 0) {
+        let newString = '';
+        for (let i = 0; i < num; i++) {
+            newString += string;
+        }
+        return newString;
+    }
+    else return "ERROR";
 };
 
 // Do not edit below this line
