0b5076f60a7aafa934f73a5c62826a85ac786996
Dylan <dmurayama93@gmail.com>
2023-09-21 18:50:27 -1000

    add pseudocode for 03_reverseString

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,7 @@
 const reverseString = function() {
-
+// take string and store each character into array
+// slice off last letter of array and store in new array
+//return new array
 };
 
 // Do not edit below this line
