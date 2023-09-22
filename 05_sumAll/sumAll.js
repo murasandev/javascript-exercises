9356916f768e2759fb03672c32b3b92b94ed47a7
Dylan <dmurayama93@gmail.com>
2023-09-22 09:48:02 -1000

    complete 05 sum all

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,25 @@
-const sumAll = function() {
+const sumAll = function(...numbers) {
+    numbers.sort();
+    let checkNaN = false;
+    // const newArray = [];
+    for (let i = 0; i <= numbers.length -1; i++) {
+        if (!Number.isInteger(numbers[i])){
+            checkNaN = true;
+            return "ERROR";
+        }
+    }
 
+    if (numbers[0] >= 0 && !checkNaN) {
+        let j = numbers[0];
+
+        for (let i = numbers[0] + 1; i <= numbers[numbers.length - 1]; i++) {
+            j += i;
+        }
+        return j;
+    }
+    else {
+        return "ERROR";
+    }
 };
 
 // Do not edit below this line
