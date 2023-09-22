1cb5ced2b24ae10855a4f732fa118fa757511002
Dylan <dmurayama93@gmail.com>
2023-09-22 08:36:01 -1000

    removes comments on 04 remove from array

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,15 +1,12 @@
 const removeFromArray = (array, ...removeItems) => {
-// take array, remove item from array
-// loop through array and check items to remove
-    // for each item in array 
-    // loop through removeitems and compare if match
-    // if no match, slice to new array
+
     const sortedArray = [];
+
     let isMatching = false;
+
     for (const item of array) {
         for (const itemCheck of removeItems) {
             if (item === itemCheck) {
-                console.log(`remove item: ${item}`);
                 isMatching = true;
             }
         }
