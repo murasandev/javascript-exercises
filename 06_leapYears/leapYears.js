493325a0dac028c59afeb1b2d959c13e70dace75
Dylan <dmurayama93@gmail.com>
2023-09-22 17:27:37 -1000

    complete 07 temp conversion

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,12 @@
-const leapYears = function() {
+const leapYears = function(year) {
+    let isLeapYear;
 
+    if (year % 100 === 0) {
+        isLeapYear = year => (year % 400 === 0) ? true : false;
+    }
+    else {isLeapYear = (year) => (year % 4 === 0) ? true : false;}
+    
+    return isLeapYear(year);
 };
 
 // Do not edit below this line
--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(degrees) {
+    degrees = (degrees - 32) * (5/9);
+    return (Math.round(degrees * 10)/10);
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(degrees) {
+    degrees = (degrees * (9/5) +32);
+    return (Math.round(degrees * 10)/10);
 };
 
 // Do not edit below this line
