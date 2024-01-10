2340a4fba1b7cc22858bd5acbacf9b957fc32993
Dylan <dmurayama93@gmail.com>
2024-01-09 18:26:22 -1000

    psuedo code palindrome logic

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,8 @@
 const palindromes = function () {
-
+// take out spaces and punctuation
+// convert to all lower-case
+// store values into array[1] and store values from reverse string in array[2]
+// compare array values, if all equal return palindrome
 };
 
 // Do not edit below this line
