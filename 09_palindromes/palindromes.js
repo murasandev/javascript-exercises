48c744fbaa2b8f01fcf5abc2936dedcdb8c59a94
Dylan <dmurayama93@gmail.com>
2024-01-09 18:28:41 -1000

    add psuedo for removing punctuation

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -3,6 +3,9 @@ const palindromes = function () {
 // convert to all lower-case
 // store values into array[1] and store values from reverse string in array[2]
 // compare array values, if all equal return palindrome
+
+// try removing punctuation by converting to lowercase than if value of char 
+// not within lowercase values, omit
 };
 
 // Do not edit below this line
