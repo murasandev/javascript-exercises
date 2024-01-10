142b153f83664bceb5eba7283c24dbb150327f25
Dylan <dmurayama93@gmail.com>
2024-01-09 19:47:04 -1000

    fix fib logic

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,19 +1,20 @@
 const fibonacci = function(n) {
     // get total ex n = 3, 1,1,2,3 i[1] = 1, i[2] = 1,
-    let total = 1,
-        currNumber = 1,
+    let currNumber = 1,
         nextNumber = 0;
-        
-    let numberArray = [0];
 
-    return (n === 1 ? 1 : (() => {
-        for (i = 1; i <= n; i++) {
-            currNumber = nextNumber + total;
-            numberArray[i] = currNumber;
-            nextNumber = numberArray[i-1] + currNumber;
+    let numberArray = [0, 1];
+
+    if (n === 0) return 0;
+    if (n === 1) return 1;
+
+    for (i = 2; i <= n; i++) {
+            let newNumber = currNumber + nextNumber;
+            numberArray.push(newNumber);
+            nextNumber = currNumber;
+            currNumber = newNumber;
         }
-        return numberArray[n];
-    })) 
+    return numberArray[n]; 
 };
 
 // Do not edit below this line
