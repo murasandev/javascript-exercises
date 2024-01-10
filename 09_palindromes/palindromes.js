54e242380d635b42dcbdfc6daafb0837e7a6e3e8
Dylan <dmurayama93@gmail.com>
2024-01-09 18:40:18 -1000

    convert string to lowercase, remove punctuation and spaces and store in array

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,4 +1,4 @@
-const palindromes = function () {
+const palindromes = function (testString) {
 // take out spaces and punctuation
 // convert to all lower-case
 // store values into array[1] and store values from reverse string in array[2]
@@ -6,6 +6,14 @@ const palindromes = function () {
 
 // try removing punctuation by converting to lowercase than if value of char 
 // not within lowercase values, omit
+
+let lowerCaseString = testString.toLowerCase();
+let origString = [];
+for (i = 0; i < lowerCaseString.length(); i++) {
+    if (lowerCaseString.charCodeAt(i) > 97 && lowerCaseString.charCodeAt(i) < 122) {
+        origString.push(lowerCaseString[i]);
+    }
+}
 };
 
 // Do not edit below this line
