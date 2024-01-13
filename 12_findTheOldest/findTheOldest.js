7b53fd3a543774c6577d67dbe6b4d09b7ccced1c
Dylan Murayama <dmurayama93@gmail.com>
2024-01-13 11:01:55 -1000

    complete find oldest function

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,14 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(array) {
+    return array.reduce(person => {
+        person.yearOfDeath - person.yearOfBirth;
+    })
 };
+
+const getAge = function(birth, death){
+    if(!death) death = Date().getFullYear();
+
+    return death - birth;
+}
 
 // Do not edit below this line
 module.exports = findTheOldest;
