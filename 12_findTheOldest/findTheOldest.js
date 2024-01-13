87c911c0753e41b32e5114923f01b057ef29ea3c
Dylan Murayama <dmurayama93@gmail.com>
2024-01-13 11:05:13 -1000

    finish find oldest function

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,7 +1,12 @@
 const findTheOldest = function(array) {
-    return array.reduce(person => {
-        person.yearOfDeath - person.yearOfBirth;
-    })
+    return array.reduce((oldest, currentPerson) => {
+        const oldestAge = getAge(oldest.yearOfBirth, oldest.yearOfDeath);
+        const currentAge = getAge(
+          currentPerson.yearOfBirth,
+          currentPerson.yearOfDeath
+        );
+        return oldestAge < currentAge ? currentPerson : oldest;
+      });
 };
 
 const getAge = function(birth, death){
