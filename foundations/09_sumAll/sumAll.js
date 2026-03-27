27b3343e6045285c2588cea413582da838aa9879
Dylan M <dmurayama93@gmail.com>
2026-03-27 13:54:54 -1000

    complete sumAll

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,5 +1,22 @@
-const sumAll = function() {
-
+const sumAll = function(min, max) {
+  let newArr = [];
+  if(!Number.isInteger(min) || !Number.isInteger(max) || min < 0 || max < 0){
+    return "ERROR";
+  }
+  else{
+    if(min < max){
+      for (let index = min; index <= max; index++) {
+        newArr.push(index);
+      }
+    }
+    else{
+      for (let index = max; index <= min; index++) {
+        newArr.push(index);
+      }
+    }
+    
+    return newArr.reduce((total, current) => total + current);
+  }
 };
 
 // Do not edit below this line
