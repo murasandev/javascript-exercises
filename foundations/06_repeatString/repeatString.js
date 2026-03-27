578d61a3bcefbc6fa6e03c3e7a6e150be8d908fc
Dylan M <dmurayama93@gmail.com>
2026-03-27 13:12:55 -1000

    complete repeatStrings

--- foundations/06_repeatString/repeatString.js
+++ foundations/06_repeatString/repeatString.js
@@ -1,5 +1,15 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+  let newString = '';
+  if(num < 0){
+    return "ERROR";
+  }
+  else{
+    for (let index = 0; index < num; index++) {
+      newString += string;
+      
+    }
+  }
+  return newString;
 };
 
 // Do not edit below this line
