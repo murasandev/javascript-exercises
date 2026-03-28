3dceb8e95d241e9687fa20fd22cc6f7916b10ff9
Dylan M <dmurayama93@gmail.com>
2026-03-27 14:11:50 -1000

    complete leapyears

--- foundations/10_leapYears/leapYears.js
+++ foundations/10_leapYears/leapYears.js
@@ -1,5 +1,24 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+// leap years divisible by 4
+// if divisible by 100 not leap
+// unless divisible by 400
+  if(year % 4 === 0){
+    // possible leap
+    if(year % 100 != 0){
+      return true;
+    }
+    else if (year % 100 === 0){
+      if(year % 400 != 0){
+        return false;
+      }
+      else{
+        return true;
+      }
+    }
+  }
+  else{
+    return false;
+  }
 };
 
 // Do not edit below this line
